bf12b486cfabfec4b2e011b33904ccb4d64493b2
aconkli9 <aconkli9@ford.com>
2022-07-13 15:04:44 -0400

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import world from './world.png'
+import data from './data'
 
-function App() {
+function App(data) {
+  // const locations = data.map(item => {
+
+  // })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className='header--container'>
+          <img src={world} alt='World Icon' />
+          my travel journal
+        </div>
       </header>
+      <div className='card'>
+        <img className='hero--img' src='https://source.unsplash.com/WLxQvbMyfas' alt='Mount Fuji'></img>
+        <div className='card--info'>
+          <div className='maps--link'>
+            <img src='https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/map-marker-icon.png' alt="marker-icon"></img>
+            <p>JAPAN </p>
+            <a href='https://www.google.com/maps?q=mount+fuji&rlz=1C1GCEB_enUS931US931&um=1&ie=UTF-8&sa=X&ved=2ahUKEwix-Yyz-vX4AhVOEEQIHVVbA7IQ_AUoA3oECAIQBQ'>View in Google Maps</a>
+          </div>
+          <h1>Mount Fuji</h1>
+          <p>12 Jan, 2021 - 24 Jan, 2021</p>
+          <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    title: 'Mount Fuji',
+    location: 'Japan',
+    maps: 'https://www.google.com/maps?q=mount+fuji&rlz=1C1GCEB_enUS931US931&um=1&ie=UTF-8&sa=X&ved=2ahUKEwix-Yyz-vX4AhVOEEQIHVVbA7IQ_AUoA3oECAIQBQ',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
+    imageURL: 'https://source.unsplash.com/WLxQvbMyfas'
+  },
+  {
+    title: 'Syndey Opera House',
+    location: 'Australia',
+    maps: 'https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967',
+    startDate: '27 May, 2021',
+    endDate: '8 Jun, 2021',
+    description: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th centurys most famous and distinctive buildings',
+    imageURL: 'https://source.unsplash.com/JmuyB_LibRo'
+  },
+  {
+    title: 'Geirangerfjord',
+    location: 'Norway',
+    maps: 'https://www.google.com/maps/place/Geirangerfjord/@62.104831,7.0051743,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817',
+    startDate: '01 oct, 2021',
+    endDate: '18 Nov, 2021',
+    description: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
+    imageURL: 'https://source.unsplash.com/WLxQvbMyfas'
+  },
+]
+
+export default data
